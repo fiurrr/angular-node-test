a3a3326336e3b68ba5acad2334d72727daae01cf
Jakub <wieczorekwww@gmail.com>
2015-10-26 18:43:33 +0000

    Installed ngMaterial, so it will look better

--- client/app/app.js
+++ client/app/app.js
@@ -4,7 +4,8 @@ angular.module('testApp', [
   'ngCookies',
   'ngResource',
   'ngSanitize',
-  'ngRoute'
+  'ngRoute',
+  'ngMaterial'
 ])
   .config(function ($routeProvider, $locationProvider) {
     $routeProvider
